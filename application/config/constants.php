42e66a68a0bc845cffe5cefa1a1a2b76015f411a
YeongMin <jamesome@naver.com>
2017-05-17 11:56:35 +0900

    상수 선언

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,20 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+|--------------------------------------------------------------------------
+| 상수 선언
+|--------------------------------------------------------------------------
+|
+| EX)
+| define('BOARD_NAME', 'hello world'); 출력 echo BOARD_NAME;
+|
+*/
+define('ADMIN_LEVEL', 100); // 어드민 레벨
+// define('THUMB_SIZE',       // 썸네일 사이즈
+//     array(
+//         "mini" => array(100, 75),
+//         "photo" => array(173, 129)
+//     )
+// );
