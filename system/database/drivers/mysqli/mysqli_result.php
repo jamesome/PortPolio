81f0a538b0dad8ef4464db3c85a53fe62af44c18
YeongMin <jamesome@naver.com>
2017-05-18 18:59:49 +0900

    DB 함수 추가

--- system/database/drivers/mysqli/mysqli_result.php
+++ system/database/drivers/mysqli/mysqli_result.php
@@ -183,4 +183,19 @@ class CI_DB_mysqli_result extends CI_DB_result {
 		return $this->result_id->fetch_object($class_name);
 	}
 
+	// --------------------------------------------------------------------
+
+    /**
+     * Read the next result
+     * 2017.05.18 function add
+     *
+     * @return  null
+     */
+    public function next_result()
+    {
+        if (is_object($this->conn_id)) {
+            return mysqli_next_result($this->conn_id);
+        }
+    }
+
 }
